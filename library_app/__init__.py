8811b41885e8adad87bd82778a2c9976591551a5
lordwerneo <lordwerneo@gmail.com>
2021-12-21 02:02:38 +0200

    Add blueprint. Add views/main.py as blueprint for index page.

--- library_app/__init__.py
+++ library_app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config, MIGRATION_DIR
+from library_app.views import main
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, directory=MIGRATION_DIR)
 
+app.register_blueprint(main, url_prefix='')
